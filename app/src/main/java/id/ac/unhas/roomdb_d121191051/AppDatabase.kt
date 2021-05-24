f6a533d905fc0775d7cc80ee4b7a9c4389e0c2f6
Reinhart Wibisono Soplantila <reinhartsoplantila@gmail.com>
2021-05-24 09:26:45 +0800

    Note.kt Done

--- app/src/main/java/id/ac/unhas/roomdb_d121191051/AppDatabase.kt
+++ app/src/main/java/id/ac/unhas/roomdb_d121191051/AppDatabase.kt
@@ -1,6 +1,8 @@
 package id.ac.unhas.roomdb_d121191051
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 
 @Database(entities = [Note::class], exportSchema = false, version = 1)
 abstract class AppDatabase RoomDatabase() {
